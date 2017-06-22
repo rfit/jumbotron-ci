817105b2a6c2effd3123a3ab0b875b395cf19e85
Rasmus Ljungmann Pedersen <rasmuslp@gmail.com>
2017-06-22 12:52:14 +0200

    feat: env vars can now be checked synchronously

--- src/environment.js
+++ src/environment.js
@@ -3,36 +3,47 @@ require('dotenv').config({path: process.cwd()});
 
 function ensure(vars) {
 	return new Promise((resolve, reject) => {
-		vars.forEach(v => {
-			// The env var to ensure is set in some way
-			const envVarName = v.toUpperCase();
+		const err = ensureSync(vars);
 
-			// Override generic with 4 prefixed env var
-			let pre4envVarName = process.env.TARGET_ENV ? process.env.TARGET_ENV.substr(0, 4).toUpperCase() + '_' : '';
-			pre4envVarName += v.toUpperCase();
-
-			const pre4envValue = process.env[pre4envVarName];
-			if (pre4envValue) {
-				process.env[envVarName] = pre4envValue;
-			}
-
-			// Override generic with 3 prefixed env var
-			let pre3envVarName = process.env.TARGET_ENV ? process.env.TARGET_ENV.substr(0, 3).toUpperCase() + '_' : '';
-			pre3envVarName += v.toUpperCase();
-
-			const pre3envValue = process.env[pre3envVarName];
-			if (pre3envValue) {
-				process.env[envVarName] = pre3envValue;
-			}
-
-			// Check generic is set
-			if (!process.env[envVarName]) {
-				reject(`Missing environment variable: '${envVarName}'`);
-			}
-		});
+		if (err) {
+			reject(err);
+		}
 
 		resolve();
 	});
+}
+
+function ensureSync(vars) {
+	for (let v of vars) {
+		// The env var to ensure is set in some way
+		const envVarName = v.toUpperCase();
+
+		// Override generic with 4 prefixed env var
+		let pre4envVarName = process.env.TARGET_ENV ? process.env.TARGET_ENV.substr(0, 4).toUpperCase() + '_' : '';
+		pre4envVarName += v.toUpperCase();
+
+		const pre4envValue = process.env[pre4envVarName];
+		if (pre4envValue) {
+			process.env[envVarName] = pre4envValue;
+		}
+
+		// Override generic with 3 prefixed env var
+		let pre3envVarName = process.env.TARGET_ENV ? process.env.TARGET_ENV.substr(0, 3).toUpperCase() + '_' : '';
+		pre3envVarName += v.toUpperCase();
+
+		const pre3envValue = process.env[pre3envVarName];
+		if (pre3envValue) {
+			process.env[envVarName] = pre3envValue;
+		}
+
+		// Check generic is set
+		if (!process.env[envVarName]) {
+			return `Missing environment variable: '${envVarName}'`;
+		}
+	}
+
+	// A-okay, return no nothing!
+	return;
 }
 
 function hasTargetEnvironment() {
@@ -45,5 +56,6 @@ function hasTargetEnvironment() {
 
 module.exports = {
 	ensure,
+	ensureSync,
 	hasTargetEnvironment
 };
