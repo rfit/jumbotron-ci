090c1ce043a3c0674a0a87f9547b92edf773ca46
Rasmus Ljungmann Pedersen <rasmuslp@gmail.com>
2017-06-24 01:33:22 +0200

    feat(util.exec): Collect stdout and stderr

--- src/utils.js
+++ src/utils.js
@@ -4,23 +4,40 @@ const fs = require('fs');
 // Run commands in the shell
 function exec(cmd, args, options){
 	return new Promise((resolve, reject) => {
-		const opts = Object.assign({}, {stdio: 'inherit'}, options);
 		//console.log('utils.exec', cmd, args);
 
 		// Spawn process
-		const p = childProcess.spawn(cmd, args, opts);
+		const p = childProcess.spawn(cmd, args, options);
+
+		// Collect output
+		let stdout = '';
+		p.stdout.on('data', data => {
+			console.log(data.toString());
+			stdout += data;
+		});
+
+		let stderr = '';
+		p.stdout.on('data', data => {
+			console.log(data.toString());
+			stderr += data;
+		});
 
 		// Wait for exit
 		p.on('exit', code => {
 			if (code !== 0) {
 				const err = new Error(`command "${cmd}" exited with wrong status code: ${code}`);
-				err.code = code;
-				err.cmd = cmd;
+				Object.assign(err, {
+					code,
+					cmd,
+					args,
+					stdout,
+					stderr
+				});
 
 				return reject(err);
 			}
 
-			return resolve();
+			return resolve(stdout);
 		});
 	});
 }
